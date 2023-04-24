4572b630e4c3fc7eb9ba713b2c7c85b0a7c0f56c
DragonZZ <1451590580@qq.com>
2023-04-25 01:02:55 +0800

    Some Modify on Experiment4

--- Experiment4/main.c
+++ Experiment4/main.c
@@ -35,13 +35,13 @@ void CLOCK(int total_block);
 
 int main(){
     srand(10 * getpid());
-    int random_point = (int)(319.0 * rand()/RAND_MAX) + 1;
+    int random_point = (int)(309.0 * rand()/RAND_MAX) + 1;
     for(int i = 0 ; i < 320 ; i += 4){
         instruction[i].value = random_point;
         instruction[i+1].value = random_point + 1;
         instruction[i+2].value = (int)(1.0 * instruction[i].value * rand() / RAND_MAX);
         instruction[i+3].value = instruction[i+2].value + 1;
-        random_point = (int)(1.0 * rand() * (318 - instruction[i+2].value)/RAND_MAX) + instruction[i+2].value + 2;
+        random_point = (int)(1.0 * rand() * (308 - instruction[i+2].value)/RAND_MAX) + instruction[i+2].value + 2;
     }
 
     for(int i = 0 ; i < 320 ; i++){
@@ -49,7 +49,13 @@ int main(){
         instruction[i].page_offset = instruction[i].value % 10;
     }
 
-    LFU(4);
+    for(int i = 4 ; i <= 32 ; i++){
+        FIFO(i);
+        LRU(i);
+        LFU(i);
+        CLOCK(i);
+        printf("\n");
+    }
 
     return 0;
 }
@@ -66,8 +72,8 @@ void initialize(int total_block){
         block[i-1].block_number = i - 1;
         block[i-1].next = &block[i];
     }
-    block[31].block_number = 31;
-    block[31].next = NULL;
+    block[total_block - 1].block_number = total_block - 1;
+    block[total_block - 1].next = NULL;
     free_block_head = &block[0];
 }
 
@@ -101,7 +107,7 @@ void FIFO(int total_block){
             free_block_head = tmp;
         }
     }
-    printf("FIFO: %6.4lf\n", 1 - (double)diseffect/320);
+    printf("FIFO: %6.4lf\t", 1 - (double)diseffect/320);
 }
 
 void LRU(int total_block){
@@ -117,17 +123,25 @@ void LRU(int total_block){
                     if(page_table[i].exist_flag == 0) continue;
                     min_time = min_time < page_table[i].time ? min_time : page_table[i].time;
                 }
-                struct Block *pre,*now;
+                struct Block *pre, *now;
                 now = busy_block_head;
                 pre = NULL;
                 while(now != NULL){
                     if(page_table[now->page_number].time == min_time){
-                        if(pre == NULL){
+                        if(now == busy_block_head){
                             struct Block *tmp;
                             tmp = busy_block_head->next;
                             free_block_head = busy_block_head;
                             free_block_head->next = NULL;
                             busy_block_head = tmp;
+                            page_table[free_block_head->page_number].block_number = -1;
+                            page_table[free_block_head->page_number].exist_flag = 0;
+                        }
+                        else if(now == busy_block_tail){
+                            busy_block_tail = pre;
+                            busy_block_tail->next = NULL;
+                            free_block_head = now;
+                            free_block_head->next = NULL;
                             page_table[free_block_head->page_number].block_number = -1;
                             page_table[free_block_head->page_number].exist_flag = 0;
                         }
@@ -163,7 +177,7 @@ void LRU(int total_block){
             page_table[instruction[i].page_number].time = i;
         }
     }
-    printf("LRU: %6.4lf\n",1 - (double)diseffect/320);
+    printf("LRU: %6.4lf\t",1 - (double)diseffect/320);
 }
 
 void OPT(int total_block){
@@ -188,12 +202,20 @@ void LFU(int total_block){
                 pre = NULL;
                 while(now != NULL){
                     if(page_table[now->page_number].counter == min_counter){
-                        if(pre == NULL){
+                        if(now == busy_block_head){
                             struct Block *tmp;
                             tmp = busy_block_head->next;
                             free_block_head = busy_block_head;
                             free_block_head->next = NULL;
                             busy_block_head = tmp;
+                            page_table[free_block_head->page_number].block_number = -1;
+                            page_table[free_block_head->page_number].exist_flag = 0;
+                        }
+                        else if(now == busy_block_tail){
+                            busy_block_tail = pre;
+                            busy_block_tail->next = NULL;
+                            free_block_head = now;
+                            free_block_head->next = NULL;
                             page_table[free_block_head->page_number].block_number = -1;
                             page_table[free_block_head->page_number].exist_flag = 0;
                         }
@@ -229,9 +251,76 @@ void LFU(int total_block){
             page_table[instruction[i].page_number].counter++;
         }
     }
-    printf("LFU: %6.4lf\n",1 - (double)diseffect/320);
+    printf("LFU: %6.4lf\t",1 - (double)diseffect/320);
 }
 
 void CLOCK(int total_block){
-
+    int diseffect = 0;
+    initialize(total_block);
+    busy_block_head = busy_block_tail = NULL;
+    for(int i = 0 ; i < 320 ; i++){
+        if(page_table[instruction[i].page_number].block_number == -1){
+            diseffect++;
+            if(free_block_head == NULL){
+                struct Block *pre, *now;
+                now = busy_block_head;
+                pre = NULL;
+                while(now != NULL){
+                    if(page_table[now->page_number].counter == 0){
+                        if(now == busy_block_head){
+                            struct Block *tmp;
+                            tmp = busy_block_head->next;
+                            free_block_head = busy_block_head;
+                            free_block_head->next = NULL;
+                            busy_block_head = tmp;
+                            page_table[free_block_head->page_number].block_number = -1;
+                            page_table[free_block_head->page_number].exist_flag = 0;
+                        }
+                        else if(now == busy_block_tail){
+                            busy_block_tail = pre;
+                            busy_block_tail->next = NULL;
+                            free_block_head = now;
+                            free_block_head->next = NULL;
+                            page_table[free_block_head->page_number].block_number = -1;
+                            page_table[free_block_head->page_number].exist_flag = 0;
+                        }
+                        else{
+                            pre->next = now->next;
+                            free_block_head = now;
+                            free_block_head->next = NULL;
+                            page_table[free_block_head->page_number].block_number = -1;
+                            page_table[free_block_head->page_number].exist_flag = 0;
+                        }
+                    }
+                    else{
+                        page_table[now->page_number].counter = 0;
+                    }
+                    if(now == busy_block_tail){
+                        now = busy_block_head;
+                    }
+                    pre = now;
+                    now = now->next;
+                }
+            }
+            struct Block *tmp;
+            tmp = free_block_head->next;
+            free_block_head->page_number = instruction[i].page_number;
+            free_block_head->next = NULL;
+            page_table[instruction[i].page_number].block_number = free_block_head->block_number;
+            page_table[instruction[i].page_number].counter = 1;
+            page_table[instruction[i].page_number].exist_flag = 1;
+            if(busy_block_tail == NULL){
+                busy_block_head = busy_block_tail = free_block_head;
+            }
+            else{
+                busy_block_tail->next = free_block_head;
+                busy_block_tail = free_block_head;
+            }
+            free_block_head = tmp;
+        }
+        else{
+            page_table[instruction[i].page_number].counter = 1;
+        }
+    }
+    printf("CLOCK: %6.4lf\t",1 - (double)diseffect/320);
 }
