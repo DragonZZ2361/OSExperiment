c17ae628e06bf5e120133a7de65ada6e10709885
DragonZZ <1451590580@qq.com>
2023-04-28 15:50:08 +0800

    Maybe there is some difference

--- Experiment4/main.c
+++ Experiment4/main.c
@@ -50,11 +50,14 @@ int main(){
     }
 
     for(int i = 4 ; i <= 32 ; i++){
+        printf("-------------------------------block number = %d---------------------------------\n\n", i);
         FIFO(i);
+        OPT(i);
         LRU(i);
         LFU(i);
         CLOCK(i);
         printf("\n");
+        printf("---------------------------------------------------------------------------------\n\n\nt");
     }
 
     return 0;
@@ -181,7 +184,78 @@ void LRU(int total_block){
 }
 
 void OPT(int total_block){
-
+    int diseffect = 0;
+    initialize(total_block);
+    busy_block_head = busy_block_tail = NULL;
+    for(int i = 0 ; i < 320 ; i++){
+        if(page_table[instruction[i].page_number].block_number == -1){
+            diseffect++;
+            if(free_block_head == NULL){
+                int max_time = 0;
+                for(int i = 0 ; i < 32 ; i++){
+                    if(page_table[i].exist_flag == 0) continue;
+                    max_time = max_time > page_table[i].time ? max_time : page_table[i].time;
+                }
+                struct Block *pre, *now;
+                now = busy_block_head;
+                pre = NULL;
+                while(now != NULL){
+                    if(page_table[now->page_number].time == max_time){
+                        if(now == busy_block_head){
+                            struct Block *tmp;
+                            tmp = busy_block_head->next;
+                            free_block_head = busy_block_head;
+                            free_block_head->next = NULL;
+                            busy_block_head = tmp;
+                            page_table[free_block_head->page_number].block_number = -1;
+                            page_table[free_block_head->page_number].exist_flag = 0;
+                        }
+                        else if(now == busy_block_tail){
+                            busy_block_tail = pre;
+                            busy_block_tail->next = NULL;
+                            free_block_head = now;
+                            free_block_head->next = NULL;
+                            page_table[free_block_head->page_number].block_number = -1;
+                            page_table[free_block_head->page_number].exist_flag = 0;
+                        }
+                        else{
+                            pre->next = now->next;
+                            free_block_head = now;
+                            free_block_head->next = NULL;
+                            page_table[free_block_head->page_number].block_number = -1;
+                            page_table[free_block_head->page_number].exist_flag = 0;
+                        }
+                    }
+                    pre = now;
+                    now = now->next;
+                }
+            }
+            struct Block *tmp;
+            tmp = free_block_head->next;
+            free_block_head->page_number = instruction[i].page_number;
+            free_block_head->next = NULL;
+            page_table[instruction[i].page_number].block_number = free_block_head->block_number;
+            page_table[instruction[i].page_number].time = 0x3f3f3f3f;
+            page_table[instruction[i].page_number].exist_flag = 1;
+            for(int j = i + 1 ; j < 320 ; j++){
+                if(instruction[i].page_number == instruction[j].page_number){
+                    page_table[instruction[i].page_number].time = j+1-i;
+                }
+            }
+            if(busy_block_tail == NULL){
+                busy_block_head = busy_block_tail = free_block_head;
+            }
+            else{
+                busy_block_tail->next = free_block_head;
+                busy_block_tail = free_block_head;
+            }
+            free_block_head = tmp;
+        }
+        else{
+            page_table[instruction[i].page_number].counter++;
+        }
+    }
+    printf("OPT: %6.4lf\t",1 - (double)diseffect/320);
 }
 
 void LFU(int total_block){
